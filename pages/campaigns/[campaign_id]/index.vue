9f3b8580c65fd165334907877aab9035f14a4e39
Mrdeck <mrdeck30@gmail.com>
2024-02-17 14:53:33 +0100

    stash

--- pages/campaigns/[campaign_id]/index.vue
+++ pages/campaigns/[campaign_id]/index.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container>
+<<<<<<< Updated upstream
     <v-card class="pa-5">
       <v-form ref="formRef" @submit.prevent="onFormSubmit">
         <v-file-input
@@ -26,6 +27,31 @@
               >
                 {{ fileName }}
               </v-chip>
+=======
+    <v-file-input
+      v-model="files"
+      color="deep-purple-accent-4"
+      counter
+      label="File input"
+      placeholder="Select your files"
+      prepend-icon="mdi-paperclip"
+      variant="outlined"
+      :show-size="1000"
+      accept=".pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      @change="onFileInputChange"
+    >
+      <template v-slot:selection="{ fileNames }">
+        <template v-for="(fileName, index) in fileNames" :key="fileName">
+          <v-chip
+            v-if="index < 2"
+            color="deep-purple-accent-4"
+            label
+            size="small"
+            class="me-2"
+          >
+            {{ fileName }}
+          </v-chip>
+>>>>>>> Stashed changes
 
               <span
                 v-else-if="index === 2"
@@ -40,6 +66,7 @@
           <v-btn type="submit" v-on:submit.prevent="onFormSubmit"
             >Soumettre</v-btn
           >
+<<<<<<< Updated upstream
         </div>
       </v-form>
       <div class="text-center mt-10">
@@ -50,16 +77,29 @@
         >
       </div>
     </v-card>
+=======
+            +{{ files.length - 2 }} File(s)
+          </span>
+        </template>
+      </template>
+    </v-file-input>
+    <img :src="fileUrl" alt="file-uploaded" />
+>>>>>>> Stashed changes
   </v-container>
 </template>
 <script lang="ts" setup>
 import fs from "fs";
+<<<<<<< Updated upstream
 
+=======
+import pdf from "pdf-parse";
+>>>>>>> Stashed changes
 const route = useRoute();
 definePageMeta({
   layout: "user",
 });
 
+<<<<<<< Updated upstream
 const { $toast } = useNuxtApp();
 
 const onFileUploadChange = () => {
@@ -126,4 +166,45 @@ async function onFormSubmit(e: Event) {
   //   console.log(error.value);
   // }
 }
+=======
+const files = ref([]);
+const fileUrl = ref("");
+console.log(route.params.campaign_id);
+
+const onFileInputChange = async () => {
+  console.log(files.value[0]);
+  fileUrl.value = URL.createObjectURL(files.value[0]);
+  console.log(fileUrl.value);
+};
+
+async function extractTextFromPDF(pdfPath) {
+  const response = await fetch(pdfPath);
+  const buffer = await response.arrayBuffer();
+
+  const data = new Uint8Array(buffer);
+  const text = await parse(data);
+
+  console.log(text.text); // Affiche le texte extrait du PDF
+}
+
+// async function extractText(file: File) {
+//   fileUrl.value = URL.createObjectURL(file);
+//   // Charger le fichier PDF
+//   const pdf = await pdfjsLib.getDocument(fileUrl.value).promise;
+
+//   // Obtenir le nombre de pages
+//   const numPages = await pdf.numPages;
+
+//   // Extraire le texte de chaque page
+//   let text = "";
+//   for (let i = 1; i <= numPages; i++) {
+//     const page = await pdf.getPage(i);
+//     const textContent = await page.getTextContent();
+//     text += textContent.items.map((item) => item.str).join("\n");
+//   }
+
+//   // Retourner le texte extrait
+//   return text;
+// }
+>>>>>>> Stashed changes
 </script>
